d4a6e9cb9ac529e87320c18e4e55ecff7e63d2ba
yury <serhienya19@yandex.by>
2019-03-06 00:29:57 +0300

    prototype OOP and #2

--- homework9.js
+++ homework9.js
@@ -38,3 +38,44 @@ var barsik = new Cat('Барсик');
 console.log(barsik.name);
 console.log(barsik.dailyNorm(100));
 console.log(barsik.feed());
+
+
+// ----------------------------------2------------------------
+
+
+var obj = {
+    name: 'kot',
+    age: 11,
+    food: {
+        milk: true,
+        meat: 'yes',
+        beer: {
+            white: true,
+            black: false
+        }
+    },
+    arr : [1, 2, 3, [1.1, 2.1, [3.2], 3.1], 4]
+}
+
+function clone(obj) {
+    obj = obj || this;
+    var new_obj  = {};
+
+    for( var key in obj ) {
+      if ( obj.hasOwnProperty(key) ) {
+        if( obj[key] !== null && typeof(obj[key]) === "object" ) {
+          new_obj[key] = clone( obj[key] );
+        }
+        else {
+          new_obj[key] = obj[key];
+        }
+      }
+    }
+
+    return new_obj;
+  };
+
+  var qwe = clone(obj);
+  qwe.age = 25;
+  qwe.food.meat = 'qwert';
+  console.log(qwe);
