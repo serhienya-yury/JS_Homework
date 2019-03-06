8244deb87688865f7955b77fae7a4f18b07016f4
yury <serhienya19@yandex.by>
2019-03-06 21:49:41 +0300

    prototype and #2 (improved)

--- homework9.js
+++ homework9.js
@@ -1,65 +1,72 @@
 function Animal(name) {
-    this.foodAmount = 50;
+    this._foodAmount = 50;
     this.name = name;
-}
-Animal.prototype = {
+  }
+  Animal.prototype = {
     _getFormattedFoodAmount : function() {
-        return this.foodAmount + 'гр.';
+        return this._foodAmount + 'гр.';
     },
     dailyNorm : function(amount) {
-        if (!arguments.length) return this.foodAmount;
-
+        if (!arguments.length) return this._foodAmount;
+  
         if (amount < 50 || amount > 500) {
             return 'Недопустимое количество корма.';
         }
-
-        this.foodAmount = amount;
+  
+        this._foodAmount = amount;
     },
     feed : function() {
         return 'Насыпаем в миску ' + this._getFormattedFoodAmount() + ' корма.';
     }
-}
-
-function Cat() {
+  }
+  
+  function Cat() {
     Animal.apply(this, arguments);
-}
-
-Cat.prototype = Object.create(Animal.prototype);
-Cat.prototype.constructor = Cat;
-
-Cat.prototype.feed = function () {
+  }
+  
+  Cat.prototype = Object.create(Animal.prototype);
+  Cat.prototype.constructor = Cat;
+  
+  Cat.prototype.feed = function () {
     var result = Animal.prototype.feed.apply(this, arguments);
         return result + ' Кот доволен ^_^';
-}
-
-
- 
-var barsik = new Cat('Барсик');
-console.log(barsik.name);
-console.log(barsik.dailyNorm(100));
-console.log(barsik.feed());
+  }
+  
+  
+  
+  var barsik = new Cat('Барсик');
+  console.log(barsik.name);
+  console.log(barsik.dailyNorm(100));
+  console.log(barsik.feed());
+  
 
 
 // ----------------------------------2------------------------
 
 
-var obj = {
-    name: 'kot',
-    age: 11,
-    food: {
-        milk: true,
-        meat: 'yes',
-        beer: {
-            white: true,
-            black: false
-        }
+var initialObj = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
     },
-    arr : [1, 2, 3, [1.1, 2.1, [3.2], 3.1], 4]
-}
+    method: function() {
+        alert('Hello');
+    }
+};
+console.log(initialObj);
+
 
 function clone(obj) {
-    obj = obj || this;
-    var new_obj  = {};
+    var new_obj = Array.isArray(obj) ? [] : {};
 
     for( var key in obj ) {
       if ( obj.hasOwnProperty(key) ) {
@@ -75,7 +82,9 @@ function clone(obj) {
     return new_obj;
   };
 
-  var qwe = clone(obj);
-  qwe.age = 25;
-  qwe.food.meat = 'qwert';
-  console.log(qwe);
+
+
+var copyObj = clone(initialObj);
+copyObj.object.object2.array2[1].name = 'Vasya';
+copyObj.array.push(2);
+console.log(copyObj);
